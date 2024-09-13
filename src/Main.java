5f706201fbaf16b5963503e03ff0afdb6008ee92
vedavia <veda.anandan@mail.utoronto.ca>
2024-09-13 09:56:16 -0400

    New method 'Main'

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        FizzBuzz.doFizzBuzz(5);
+        FizzBuzz.main(); // Removing the parameter doesn't directly fix the error.
+    }
+}
